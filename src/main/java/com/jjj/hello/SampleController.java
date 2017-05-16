596e697e2dad4e7bd59f3c8ba8be406d716b102b
dljjj <535530307@qq.com>
2017-05-16 13:55:33 +0800

    init

---
+++ src/main/java/com/jjj/hello/SampleController.java
@@ -0,0 +1,28 @@
+package com.jjj.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController implements EmbeddedServletContainerCustomizer {
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(80);
+	}
+}
